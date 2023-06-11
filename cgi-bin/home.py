90090b5b654e5aba97c7492d1f4f50998ac8b524
devel <devel>
2023-06-10 23:28:15 -0600

    update

--- cgi-bin/home.py
+++ cgi-bin/home.py
@@ -1,6 +1,45 @@
 #!/usr/bin/python3
+html = '''
+<html>
+<head>
+<title>DigitalAg Model Demo</title>
 
-with open('/opt/bitnami/apache/cgi-bin/my_index.html', 'r') as f:
-    html = f.readlines()
-for h in html:
-    print(h)
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link href='https://fonts.googleapis.com/css?family=Space Grotesk' rel='stylesheet'>
+<link rel="stylesheet" type="text/css" href="http://149.165.155.188:2298/css/style.css">
+  <div id="about" class="aboutContainer container-fluid">
+    <div class="AboutInfo">
+      <div class="row">
+        <div class="col-12 offset-1">
+          <div class="AboutInfoTitle">
+            DefoNet Demo<br>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="teamContainer container-fluid">
+      <div class="row">
+        <div class="col-2">
+          <div id="ScoutingMapInterpretabilityUpload" class="container-fluid personInfoContainer">
+            <div class="container-fluid personInfoStyleContainer">
+              <div class="dot"></div>
+              <div class="dot"></div>
+              <div class="dot"></div>
+            </div>
+            <div class="personInfoTextContainer">
+              <div class="aboutTextContainer">
+<a href = "/cgi-bin/run_32_32_64_64_64_128_128_128.py">defonet_32_32_64_64_64_128_128_128</a><br>
+<a href = "/cgi-bin/run_32_32_64_64_64_128_128_128.py">defonet_32_32_64_64_64_128_128_128</a><br>
+<a href = "/cgi-bin/run_32_32_64_64_64_128_128_128.py">defonet_32_32_64_64_64_128_128_128</a><br>
+<a href = "/cgi-bin/run_32_32_64_64_64_128_128_128.py">defonet_32_32_64_64_64_128_128_128</a><br>
+<a href = "/cgi-bin/run_32_32_64_64_64_128_128_128.py">defonet_32_32_64_64_64_128_128_128</a><br>
+<br>
+              </div>
+            </div>
+	  </div>
+	</div>
+</body>
+'''
+
+print(html)
